67f7c2076dc5af18907a99dedc843accfa523d0a
Mitchell van Zuijlen - IO <mjpvanzuijlen@tudelft.net>
2017-02-27 16:11:12 +0100

    website framework up and running

--- webexeperiment/experimental_setup.js
+++ webexeperiment/experimental_setup.js
@@ -1,8 +1,18 @@
+
 var curTrial = 0;
+
+//temporarty way to acces stimuli, just for testing. 
 var stimuli = ['http://www.getty.edu/museum/media/images/web/enlarge/00066001.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00055301.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00066301.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00066001.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00055301.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00066301.jpg',
+              'http://www.getty.edu/museum/media/images/web/enlarge/00066001.jpg',
               'http://www.getty.edu/museum/media/images/web/enlarge/00055301.jpg',
               'http://www.getty.edu/museum/media/images/web/enlarge/00066301.jpg']
   
+
 
 /* Start the experiment.
 Remove the instructions, and show the first ShowBetweenTrials screen*/
@@ -21,12 +31,13 @@ if (firstCall != 1) {
 var element = document.getElementById('trialholder');
 element.parentNode.removeChild(element);
 }
+
 var trialnum = curTrial;
  $('#location').append(`
 <div id="betweentrials">
     <p>Continue to the next trial</p>
     <button id="next"type="button" onclick="RunTrial()">Submit</button> 
-    <p> Trial ${trialnum} of n </p>
+    <p> Trial ${trialnum} of ${stimuli.length} </p>
 </div>
     `) }
 
@@ -39,25 +50,65 @@ var trialnum = curTrial;
 function Trial() {
 	curTrial = increase(curTrial);
 
+  //generate the html code
    $('#location').append(`
-   <div id="trialholder" style="text-align:center"> 
-   <img id="image"> 
-    <form >
-        Question 1:<br>
-        <input type="text" name="firstname" value="answer"><br>
-        Question 2:<br>
-        <input type="text" name="lastname" value="answer">
-        <br><br>
-        <button id="continue" type="button" onclick="ShowBetweenTrials()"> Continue </button>
-    </form>
-    </div>`)
-      document.getElementById('image').src = stimuli[curTrial];
+   <div id="trialholder"> 
+    <img id="image"> 
+   </div>`)
 
-}
+    // present the image 
+    var targetImage = document.getElementById('image')
+    targetImage.src =  stimuli[curTrial];
+
+      // present the image at the desired size
+      var margin = 0.95; //I want 2.5% of the window empty as a margin. 
+      
+      var targetImageHeight = targetImage.height;// image
+      var targetImageWidth = targetImage.width; //
+      var windowHeight = $(window).height();   //   browser 
+      var windowWidth  = $(window).width();   //
+      var ratio = Math.min(windowWidth / targetImageWidth, windowHeight / targetImageHeight);
+
+      if (ratio < 1) {
+        targetImageHeight = targetImageHeight*ratio*margin;
+        targetImageWidth = targetImageWidth*ratio*margin;
+        targetImage.style.width = `${targetImageWidth}px`;
+        targetImage.style.height= `${targetImageHeight}px`;
+      }
+
+        //now check where the UI will be placed (to the right if there's enough space, otherwise below)
+        // But there's no UI yet, so yea..nothing to show
+
+        /*if (windowWidth-targetImageWidth >= 400) {  //400 is an arbitrary number. I don't know the size of the UI yet.
+          
+        }
+        else {
+        } */
+          document.getElementById("trialholder").setAttribute("align", "center");
+          //this is pretty much a placeholder till i know the actual data i wanna gather 
+          $('#trialholder').append(`
+              <form >
+                  Question 1:<br>
+                  <input type="text" name="firstname" value="answer"><br>
+                  Question 2:<br>
+                  <input type="text" name="lastname" value="answer">
+                  <br><br>
+                  <button id="continue" type="button" onclick="ShowBetweenTrials()"> Continue </button>
+              </form>`)
+
+
+        }
+  
+     
+
+
 
 
 // Random small functions 
 function increase(x) {
     x++;
     return x;
-    }
+  }
+  
+// needs to go back into trial()
+  
